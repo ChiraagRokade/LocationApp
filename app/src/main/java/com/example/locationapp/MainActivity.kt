14d4059aa5a260f0c490884de2c6216e6bdf9e4d
chiraagrokadetalent <112620780+chiraagrokadetalent@users.noreply.github.com>
2025-01-07 11:22:56 +0530

    update main activity

--- app/src/main/java/com/example/locationapp/MainActivity.kt
+++ app/src/main/java/com/example/locationapp/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             LocationAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    LocationApp(
                         modifier = Modifier.padding(innerPadding)
                             )
                 }
@@ -31,17 +30,6 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-        )
-}
+fun LocationApp(modifier: Modifier) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LocationAppTheme {
-        Greeting("Android")
-    }
 }
